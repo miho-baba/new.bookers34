301113efcc41adc541d5a248bd4537d077dc2e9b
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-08-17 12:48:11 +0000

    [Finish]投稿機能の作成/モデルを準備

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
 devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
+
+get 'homes/about' => 'homes#about'
+
+get 'books' => 'books#index'
+
+get 'users' => 'users#index'
+
 end
